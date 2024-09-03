7068eab28dd6921e3789fc5c2bbc1047f6228f2d
John Doe <crijacarla@gmail.com>
2024-09-03 15:44:27 -0300

    Add explanation of the riddle

---
+++ scripts/logica_lampadas.py
@@ -0,0 +1,12 @@
+5) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em salas diferentes. 
+Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser.
+Seu objetivo é descobrir qual interruptor controla qual lâmpada.Como você faria para descobrir,
+usando apenas duas idas até uma das salas das lâmpadas,qual interruptor controla cada lâmpada? 
+
+Se eu conseguisse ir em 2 salar ao invés de 1, poderia ligar o primeiro interrupitor por alguns minutos e desligar,
+em seguida ligaria o segundo interruptor e iria até a sala de uma das lampadas
+- Se a lampada estivir ligada o segundo interrupitor está comnectado a ela
+- Se ela estiver quente o primeiro interrupitor está conectado a ela
+- Se ela estiver fria o terceiro interrupitor está ligado a ela 
+Sabando qual é o interrupitor é o daquela lampada, iria na segunda sala e faria o mesmo procedimento. A lampada
+que restar ficaria com a ultima alternativa.
