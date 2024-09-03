4a8c1ac32cd60fd6373c24b108f62d88964eca5c
John Doe <crijacarla@gmail.com>
2024-09-02 22:56:28 -0300

    Add function and return

--- scripts/sequência_Fibonacci.py
+++ scripts/sequência_Fibonacci.py
@@ -1,24 +1,17 @@
-# bibliotéca para fazer a raiz quadrada
 import math
 
-# inserir o número no terminal
-n = int(input('Digite um número qualquer: '))
+def fibonacci_sequence_number(x):
+    n = x * x
 
-# transformar em n2 para as fórmulas a seguir
-r = n * n
+    calculo_1 = (5 * n + 4)
+    calculo_2 = (5 * n - 4)
 
-# fórmulas usadas para saber se o número forma um quadrado perfeito
-calculo_1 = (5 * r + 4)
-calculo_2 = (5 * r - 4)
+    resultado_1 = math.sqrt(calculo_1)
+    resultado_2 = math.sqrt(calculo_2)
 
-# calcular a raiz quadrada dos resultados das fórmulas
-resultado_1 = math.sqrt(calculo_1)
-resultado_2 = math.sqrt(calculo_2)
+    return resultado_1 % 1 == 0 or resultado_2 % 1 == 0
 
-# fazer a divisão da raiz quadrada pra saber se o resultato é 0 e retornar True ou False
-print(resultado_1 % 1 == 0 or resultado_2 % 1 == 0)
-
-
+fibonacci_sequence_number(1)
 
 
 
