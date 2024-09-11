577be85456cb7d2dd985537e75c37b0f49d55636
John Doe <crijacarla@gmail.com>
2024-09-11 13:13:57 -0300

    Rename

---
+++ scripts/sequencia_fibonacci.py
@@ -0,0 +1,18 @@
+import math
+
+def fibonacci_sequence_number(x):
+    n = x * x
+
+    calculus_1 = (5 * n + 4)
+    calculus_2 = (5 * n - 4)
+
+    result_1 = math.sqrt(calculus_1)
+    result_2 = math.sqrt(calculus_2)
+
+    return result_1 % 1 == 0 or result_2 % 1 == 0
+
+fibonacci_sequence_number(1)
+
+
+
+
